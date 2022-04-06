44aadbe7a4a460e0fd8973ce34788b4f7e84dd41
Shawn <shawn.m.dietrich@gmail.com>
2022-04-06 12:42:04 -0400

    Mounted the apiRouter

--- server/api.js
+++ server/api.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const apiRouter = express.Router();
-
+const app = express();
+const PORT = 4001;
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 
 module.exports = apiRouter;
+
+//setup cors
+app.use(cors());
+
+//Mount the main route
+app.use('/api', apiRouter);
+
+//start server
+app.listen(PORT, () => {
+    console.log(`Server Listening On Port ${PORT}`);
+});
+
