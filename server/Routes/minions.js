72179f44ff559051d542ea634e50354078317ce8
Shawn Dietrich <shawn.m.dietrich@gmail.com>
2022-04-06 20:52:30 -0400

    minions complete except for the work

--- server/Routes/minions.js
+++ server/Routes/minions.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 const {
   getAllFromDatabase,
   getFromDatabaseById,
@@ -19,21 +19,60 @@ minionsRouter.get("/", (req, res, next) => {
 });
 
 minionsRouter.get("/:minionId", (req, res, next) => {
-    const minionId = String(req.params.minionId);
-    const minion = getFromDatabaseById('minions', minionId);
+  const minionId = String(req.params.minionId);
+  const minion = getFromDatabaseById("minions", minionId);
 
-    if (minion === null ){
-        return res.status(404).send(`id: ${minionId} not found`)
-    }
-    res.send(minion);
+  if (minion === null) {
+    return res.status(404).send(`id: ${minionId} not found`);
+  }
+  res.send(minion);
 });
 
-minionsRouter.get('/:minionId/work', (req, res, next) => {
+minionsRouter.post("/", (req, res, next) => {
+  const newMinion = {
+    name: req.body.name,
+    title: req.body.title,
+    weaknesses: req.body.weaknesses,
+    salary: Number(req.body.salary),
+  };
 
-    const work = getFromDatabaseById('work', req.params.minionId)
-    if (work === null ){
-        return res.status(404).send()
-    }
-    res.send(work)
+  if (
+    newMinion.name &&
+    newMinion.title &&
+    newMinion.weaknesses &&
+    newMinion.salary > 0
+  ) {
+    const addedMinion = addToDatabase("minions", newMinion);
+    res.send(addedMinion);
+  }
+});
+
+minionsRouter.put("/:minionId", (req, res, next) => {
+  const updateMinion = req.body;
+  Number(updateMinion.salary)
+  if (getFromDatabaseById("minions", updateMinion.id) !== -1) {
+    updateInstanceInDatabase("minions", updateMinion);
+    console.log(updateMinion)
+    res.status(200).send();
+  } else {
+    res.status(404).send();
+  }
+});
+
+minionsRouter.delete('/:minionId', (req, res, next) => {
+  if(getFromDatabaseById('minions', req.params.minionId) === -1 ){
+    res.status(404).send();
+  } else {
+    deleteFromDatabasebyId('minions', req.params.minionId)
+    res.status(200).send();
+  }
 })
 
+//bouns work
+minionsRouter.get("/:minionId/work", (req, res, next) => {
+  const work = getFromDatabaseById("work", req.params.minionId);
+  if (work === null) {
+    return res.status(404).send();
+  }
+  res.send(work);
+});
